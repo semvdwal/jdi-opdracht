0bbad9a807bc147a83d064b6e153a33eb140c824
sem <sem@sem-it.nl>
2017-10-20 09:31:56 +0200

    Added User

---
+++ app/models/User.java
@@ -0,0 +1,46 @@
+package models;
+
+import models.embedded.Address;
+
+
+public class User {
+
+    private String username;
+    private String email;
+    private String phone;
+
+    private Address address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+}
