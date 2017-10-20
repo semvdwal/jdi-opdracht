36248c24abf109e6e0673d6baf96b951f01be934
sem <sem@sem-it.nl>
2017-10-20 10:27:54 +0200

    Moved id to Model class Added inheritance

--- app/models/User.java
+++ app/models/User.java
@@ -1,16 +1,13 @@
 package models;
 
+import library.Model;
 import models.embedded.Address;
-import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Entity;
-import org.mongodb.morphia.annotations.Id;
 import org.mongodb.morphia.annotations.Reference;
 
 @Entity(value = "DB.users")
-public class User {
+public class User extends Model<User> {
 
-    @Id
-    private ObjectId _id;
     private String username;
     private String email;
     private String phone;
