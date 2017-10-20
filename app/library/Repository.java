8a929715c5614d3cfbd4ed6089be8bbbefbebcf1
sem <sem@sem-it.nl>
2017-10-20 14:58:30 +0200

    Removed generic type of Model

--- app/library/Repository.java
+++ app/library/Repository.java
@@ -12,30 +12,30 @@ abstract public class Repository<T extends Model> {
     @Inject
     protected PlayMorphia morphia;
 
-    private Class<Model<T>> modelClass;
+    private Class<T> modelClass;
 
-    public Repository(Class<Model<T>> modelClass) {
+    public Repository(Class<T> modelClass) {
         this.modelClass = modelClass;
     }
 
-    public List<Model<T>> findAll() {
+    public List<T> findAll() {
         return morphia.datastore().createQuery(modelClass).asList();
     }
 
-    public Model<T> get(ObjectId id) {
+    public T get(ObjectId id) {
         return morphia.datastore().createQuery(modelClass).field("_id").equal(id).get();
     }
 
-    public Model<T> getByKey(String key, Object value) {
+    public T getByKey(String key, Object value) {
         return morphia.datastore().createQuery(modelClass).field(key).equal(value).get();
     }
 
-    public List<Model<T>> findByKey(String key, Object value) {
+    public List<T> findByKey(String key, Object value) {
         return morphia.datastore().createQuery(modelClass).field(key).equal(value).asList();
     }
 
-    public Model<T> save(T model) {
-        Model<T> modelWithId = model.withId();
+    public Model save(T model) {
+        Model modelWithId = model.withId();
         morphia.datastore().save(modelWithId);
         return modelWithId;
     }
