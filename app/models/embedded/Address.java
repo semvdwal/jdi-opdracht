1243b90672c6cf0734a29ff571c273e8588d217d
sem <sem@sem-it.nl>
2017-10-20 09:32:19 +0200

    Added Address

---
+++ app/models/embedded/Address.java
@@ -0,0 +1,52 @@
+package models.embedded;
+
+public class Address {
+
+    private String firstName;
+    private String lastName;
+
+    private String streetName;
+    private String streetNumber;
+
+    private String city;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
