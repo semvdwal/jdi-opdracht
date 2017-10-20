4e73dac32258c2b850676d1862dd9cc595e3f3a3
sem <sem@sem-it.nl>
2017-10-20 14:52:08 +0200

    Removed repository inheritance

--- app/repositories/UserRepository.java
+++ app/repositories/UserRepository.java
@@ -1,22 +1,46 @@
 package repositories;
 
-import library.Repository;
+import it.unifi.cerm.playmorphia.PlayMorphia;
 import models.User;
+import org.bson.types.ObjectId;
 import org.mongodb.morphia.query.Query;
 
-public class UserRepository extends Repository<User> {
+import javax.inject.Inject;
+import java.util.List;
 
-    public UserRepository() {
-        super(User.class);
-    }
+public class UserRepository {
+
+    @Inject
+    PlayMorphia morphia;
 
     public User findByUserName(String name) {
         return getByKey("username", name);
     }
 
-    @Override
-    protected Query<User> getUniqueQuery(User model) {
+    private Query<User> getUniqueQuery(User model) {
         return morphia.datastore().createQuery(User.class).field("username").equal(model.getUsername());
+    }
+
+    public List<User> findAll() {
+        return morphia.datastore().createQuery(User.class).asList();
+    }
+
+    public User get(ObjectId id) {
+        return morphia.datastore().createQuery(User.class).field("_id").equal(id).get();
+    }
+
+    public User getByKey(String key, Object value) {
+        return morphia.datastore().createQuery(User.class).field(key).equal(value).get();
+    }
+
+    public List<User> findByKey(String key, Object value) {
+        return morphia.datastore().createQuery(User.class).field(key).equal(value).asList();
+    }
+
+    public User save(User model) {
+        User modelWithId = model.withId();
+        morphia.datastore().save(modelWithId);
+        return modelWithId;
     }
 
 }
