f8a2c212edd570a824bafc36721508e080372a5c
sem <sem@sem-it.nl>
2017-10-20 09:38:00 +0200

    Added annotations for morphia

--- app/models/User.java
+++ app/models/User.java
@@ -1,14 +1,21 @@
 package models;
 
 import models.embedded.Address;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Reference;
 
-
+@Entity(value = "DB.users")
 public class User {
 
+    @Id
+    private ObjectId _id;
     private String username;
     private String email;
     private String phone;
 
+    @Reference
     private Address address;
 
     public String getUsername() {
